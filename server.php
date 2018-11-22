7ffb12041c908f95dc48386bc123644f7d3c693e
Nommyde <rus@tam.pw>
2018-11-23 02:34:38 +0300

    set concurrent requests limit and queue size limit

--- server.php
+++ server.php
@@ -1,8 +1,9 @@
 <?php
 
 use Psr\Http\Message\ServerRequestInterface;
+use React\Http\Middleware\LimitConcurrentRequestsMiddleware;
 use React\Http\Response;
-use React\Http\Server;
+use React\Http\StreamingServer;
 use React\Promise\Promise;
 
 require __DIR__ . '/vendor/autoload.php';
@@ -11,12 +12,17 @@ $id = 0;
 $queue = [];
 $loop = React\EventLoop\Factory::create();
 $socket = new React\Socket\Server(8080, $loop);
-
-$server = new Server(function (ServerRequestInterface $request) use (&$queue, &$id) {
+$limiter = new LimitConcurrentRequestsMiddleware(20);
+$server = new StreamingServer([$limiter, function (ServerRequestInterface $request) use (&$queue, &$id) {
     $id++;
     $query = $request->getQueryParams();
     $important = intval($query['important'] ?? 0);
     echo "request $id" . ($important ? "!\n" : "\n");
+
+    if (!$important && count($queue) > 15) {
+        echo "  deny $id\n";
+        return new Response(200, ['Content-Type' => 'text/plain'], 'no');
+    }
 
     return new Promise(function ($resolve) use (&$queue, $important, $id) {
         if ($important) {
@@ -25,12 +31,12 @@ $server = new Server(function (ServerRequestInterface $request) use (&$queue, &$
             array_push($queue, [$resolve, $id]);
         }
     });
-});
+}]);
 
 $loop->addPeriodicTimer(5, function () use (&$queue) {
     if ([$resolve, $id] = array_shift($queue)) {
         echo "  response for $id\n";
-        $resolve(new Response(200, ['Content-Type' => 'text/plain'], 'ok'));
+        $resolve(new Response(200, ['Content-Type' => 'text/plain'], 'yes'));
     }
 });
 
